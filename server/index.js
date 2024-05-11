c029ead5783609b93ada2fbf0a6b0df7b90712ed
shashank4891 <shashanksinha45@gmail.com>
2024-05-11 18:53:38 +0530

    final changes before deployment

--- server/index.js
+++ server/index.js
@@ -1,31 +1,26 @@
 require("dotenv").config();
-
-// import express
 const express = require("express");
 const { connectToMongoDB } = require("./database");
+const path = require("path");
 
-// create an instance of express called app
 const app = express();
 app.use(express.json());
 
-// import our todos router
-const router = require("./routes");
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build/index.html"));
+})
 
-// use /api to prefix our endpoints
+
+const router = require("./routes");
 app.use("/api", router);
 
-// create a test router
-app.get("/hello", (req, res) => {
-    res.status(200).json({ mssg: "hello" });
-});
-
-// create a port variable
 const port = process.env.PORT || 5000;
 
-const startServer = async () => {
+async function startServer() {
     await connectToMongoDB();
     app.listen(port, () => {
         console.log(`Server is listening on http://localhost:${port}`);
     });
-};
+}
 startServer();
